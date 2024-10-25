7433d9f50d3ca9998710890999bb2547dbb71296
pedromain <macieirapf@gmail.com>
2024-10-25 11:32:55 -0300

    Add files via upload

---
+++ ProjetGitHb/script.js
@@ -0,0 +1,50 @@
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("fa-xmark");
+  navbar.classList.toggle("active");
+};
+
+// scroll
+
+let sections = document.querySelectorAll("section");
+let navLinks = document.querySelectorAll("header nav a");
+
+window.onscroll = () => {
+  sections.forEach((sec) => {
+    let top = window.scrollY;
+    let offset = sec.offsetTop - 150;
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach((links) => {
+        links.classList.remove("active");
+        document
+          .querySelector("header nav a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+
+let header = document.querySelector("header");
+header.classList.toggle("sticky", window.scrollY > 100);
+
+menuIcon.classList.remove("fa-xmark");
+navbar.classList.remove("active");
+
+ScrollReveal({
+  distance: "100px",
+  duration: 2000,
+  delay: 200,
+}).reveal(".home-content, .heading", { origin: "top" });
+ScrollReveal().reveal(
+  ".home-img, .services-container, .portfolio-box, .contact form",
+  {
+    origin: "bottom",
+  }
+);
+ScrollReveal().reveal(".home-content h1, .about-img", { origin: "left" });
+ScrollReveal().reveal(".home-content p, .about-content", { origin: "right" });
